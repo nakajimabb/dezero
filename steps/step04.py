3a31ca6dc1ca9d0dde79548332c19c360216fd93
中島真一 <nakajimabb@gmail.com>
2023-09-23 23:04:55 +0900

    step04 数値微分

--- steps/step04.py
+++ steps/step04.py
@@ -27,13 +27,28 @@ class Exp(Function):
         return np.exp(x)
 
 
-A = Square()
-B = Exp()
-C = Square()
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+f = Square()
+x = Variable(np.array(2.0))
+dy = numerical_diff(f, x)
+print(dy)
+
+
+def f(x):
+    A = Square()
+    B = Exp()
+    C = Square()
+    return C(B(A(x)))
+
 
 x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
+dy = numerical_diff(f, x)
 
-print(y.data)
+print(dy)
