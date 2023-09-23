54e2a739bf81a0b4d2d7adfb1a6af145fb6231fe
中島真一 <nakajimabb@gmail.com>
2023-09-23 23:06:30 +0900

    step06 手作業によるバックプロパゲーション

--- steps/step06.py
+++ steps/step06.py
@@ -4,6 +4,7 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
 
 
 class Function:
@@ -11,9 +12,13 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        self.input = input
         return output
 
     def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
         raise NotImplementedError()
 
 
@@ -21,10 +26,20 @@ class Square(Function):
     def forward(self, x):
         return x ** 2
 
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
 
 def numerical_diff(f, x, eps=1e-4):
@@ -35,10 +50,19 @@ def numerical_diff(f, x, eps=1e-4):
     return (y1.data - y0.data) / (2 * eps)
 
 
-f = Square()
-x = Variable(np.array(2.0))
-dy = numerical_diff(f, x)
-print(dy)
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
 
 
 def f(x):
@@ -48,7 +72,5 @@ def f(x):
     return C(B(A(x)))
 
 
-x = Variable(np.array(0.5))
-dy = numerical_diff(f, x)
-
-print(dy)
+print(x.grad)
+print(numerical_diff(f, x))
