551bc97b8b801a2deca5127a1370e8816776e299
中島真一 <nakajimabb@gmail.com>
2023-09-23 23:15:29 +0900

    step11 可変長の引数(順伝播編)

--- steps/step11.py
+++ steps/step11.py
@@ -1,5 +1,4 @@
 import numpy as np
-import unittest
 
 
 class Variable:
@@ -29,14 +28,15 @@ class Variable:
 
 
 class Function:
-    def __call__(self, input):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(as_array(y))
-        output.set_creater(self)
-        self.input = input
-        self.output = output
-        return output
+    def __call__(self, inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(xs)
+        outputs = [Variable(as_array(y)) for y in ys]
+        for output in outputs:
+            output.set_creater(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs
 
     def forward(self, x):
         raise NotImplementedError()
@@ -68,44 +68,26 @@ class Exp(Function):
 def square(x):
     return Square()(x)
 
+
 def exp(x):
     return Exp()(x)
+
 
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
     return x
 
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
+
+class Add(Function):
+    def forward(self, xs):
+        x0, x1 = xs
+        y = x0 + x1
+        return (y,)
 
 
-class SquareTest(unittest.TestCase):
-    def test_forward(self):
-        x = Variable(np.array(2.0))
-        y = square(x)
-        expected = np.array(4.0)
-        self.assertEqual(y.data, expected)
-
-    def test_backward(self):
-        x = Variable(np.array(3.0))
-        y = square(x)
-        y.backward()
-        expected = np.array(6.0)
-        self.assertEqual(x.grad, expected)
-
-    def test_gradient_check(self):
-        x = Variable(np.random.rand(1))
-        y = square(x)
-        y.backward()
-        num_grad = numerical_diff(square, x)
-        flg = np.allclose(x.grad, num_grad)
-        self.assertTrue(flg)
-
-
-
-unittest.main()
+xs = [Variable(np.array(2)), Variable(np.array(3))]
+f = Add()
+ys = f(xs)
+y = ys[0]
+print(y.data)
