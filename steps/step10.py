13ece1f8c3e4703f98a9c4805517b121e63528b3
中島真一 <nakajimabb@gmail.com>
2023-09-23 23:11:01 +0900

    step10 テストを行う

--- steps/step10.py
+++ steps/step10.py
@@ -1,4 +1,5 @@
 import numpy as np
+import unittest
 
 
 class Variable:
@@ -75,14 +76,36 @@ def as_array(x):
         return np.array(x)
     return x
 
-x = Variable(np.array(0.5))
-# a = square(x)
-# b = exp(a)
-# y = square(b)
-y = square(exp(square(x)))
-y.backward()
-print(x.grad)
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
 
-x = Variable(np.array(1.0))  # OK
-x = Variable(None)  # OK
-x = Variable(1.0)  # NG
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        x = Variable(np.random.rand(1))
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+
+
+
+unittest.main()
