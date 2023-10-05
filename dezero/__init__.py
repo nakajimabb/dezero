9cf58d57df99a67a5267ab4becfb9d0da17a8fa3
中島真一 <nakajimabb@gmail.com>
2023-10-05 22:41:00 +0900

    step32 高階微分(実装編)

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,4 +1,4 @@
-is_simple_core = True
+is_simple_core = False
 
 if is_simple_core:
     from dezero.core_simple import Variable
@@ -7,5 +7,12 @@ if is_simple_core:
     from dezero.core_simple import no_grad
     from dezero.core_simple import as_variable
     from dezero.core_simple import setup_variable
+else:
+    from dezero.core import Variable
+    from dezero.core import Function
+    from dezero.core import using_config
+    from dezero.core import no_grad
+    from dezero.core import as_variable
+    from dezero.core import setup_variable
 
 setup_variable()
--- dezero/core.py
+++ dezero/core.py
@@ -67,9 +67,9 @@ class Variable:
     def cleargrad(self):
         self.grad = None
 
-    def backward(self, retain_grad=False):
+    def backward(self, retain_grad=False, create_graph=False):
         if self.grad is None:
-            self.grad = np.ones_like(self.data)
+            self.grad = Variable(np.ones_like(self.data))
 
         funcs = []
         seen_set = set()
@@ -84,20 +84,21 @@ class Variable:
         while funcs:
             f = funcs.pop()
             gys = [output().grad for output in f.outputs]
-            gxs = f.backward(*gys)
-            if not isinstance(gxs, tuple):
-                gxs = (gxs,)
-            for x, gx in zip(f.inputs, gxs):
-                if x.grad is None:
-                    x.grad = gx
-                else:
-                    x.grad = x.grad + gx
-                if x.creator is not None:
-                    add_func(x.creator)
+            with using_config('enable_backprop', create_graph):
+                gxs = f.backward(*gys)
+                if not isinstance(gxs, tuple):
+                    gxs = (gxs,)
+                for x, gx in zip(f.inputs, gxs):
+                    if x.grad is None:
+                        x.grad = gx
+                    else:
+                        x.grad = x.grad + gx
+                    if x.creator is not None:
+                        add_func(x.creator)
 
-            if not retain_grad:
-                for y in f.outputs:
-                    y().grad = None  # y is weakref
+                if not retain_grad:
+                    for y in f.outputs:
+                        y().grad = None  # y is weakref
 
 
 class Function:
@@ -157,7 +158,7 @@ class Mul(Function):
         return y
 
     def backward(self, gy):
-        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        x0, x1 = self.inputs
         return gy * x1, gy * x0
 
 
@@ -203,7 +204,7 @@ class Div(Function):
         return y
 
     def backward(self, gy):
-        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        x0, x1 = self.inputs
         gx0 = gy / x1
         gx1 = gy * (-x0 / x1**2)
         return gx0, gx1
@@ -228,7 +229,7 @@ class Pow(Function):
         return y
 
     def backward(self, gy):
-        x = self.inputs[0].data
+        x, = self.inputs
         c = self.c
         return gy * c * x ** (c - 1)
 
